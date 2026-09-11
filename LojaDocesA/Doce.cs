e3212340bb50c1874a87adda213fc264198c2846
Gabriel <gabrielfuchelberguer@gmail.com>
2026-09-10 22:18:24 -0300

    feat: Entendendo o que é uma classe atributos e metodos

--- LojaDocesA/Doce.cs
+++ LojaDocesA/Doce.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace LojaDocesA
 {
-    internal class Doce
+     class Doce
     {
+        //PROPRIEDADES(ATRIBUTOS)
+        public string? Confeiteiro { get; set; }
+        public double CapacidadeKg { get; set; }
+        public double PorcentagemPronta { get; set; }
+        public double TaxaProducaoPorHora { get; set; }
+
+        //MÉTODOS ( AÇÕES )
+
+        public double CalcularkgFaltantes()
+        {
+            double porcentagemFaltante = (100.0 - PorcentagemPronta) / 100.0;
+            return CapacidadeKg * porcentagemFaltante;
+        }
+        public double CalcularTempoRestanteHoras()
+        {
+            return CalcularkgFaltantes() / TaxaProducaoPorHora;
+        }
+
+        public double CalcularValorFaltante()
+        {
+            const double PRECO_POR_KG = 45.50;
+            return CalcularkgFaltantes() * PRECO_POR_KG;
+        }
     }
 }
